feb89ab9d8e17a1ce1101eeb4af364d672dbfbad
DavidGmezHdez <davidgmezhdez@gmail.com>
2019-03-26 19:01:37 +0100

    Modificando valores

--- Práctica 2/ejercicio 4/include/utilidades.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILIDADES_H
-#define UTILIDADES_H
-
-#include <iostream>
-#include <string>
-
-int localizarSubcadena(char cadena[], char subcadena[]);
-
-#endif
--- Práctica 2/ejercicio 5/include/utilidades.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILIDADES_H
-#define UTILIDADES_H
-
-#include <iostream>
-#include <string>
-
-bool insertarCadena(char base[], char aInsertar[],int posicion,char resultado[]);
-
-#endif
